a07e0a2a957ea2f2647cc042a4b3531b785dbb7a
KenM4sters <EHCKenway@outlook.com>
2024-02-06 14:43:04 +0000

    axios impl - incomplete

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,26 @@
+import { useEffect, useState } from 'react'
 import './App.css'
+import { getGame } from './api/GameService';
 
 function App() {
+  const [data, setData] = useState({});
+  const [currentPage, setCurrentPage] = useState(0);
+
+  const getAllGames = async(page = 0, size = 10) => {
+    try {
+      setCurrentPage(page);
+      const { data } = await getGame(page, size);
+      setData(data);
+      console.log(data);
+    } catch(e) {
+      console.log(e);
+    }
+  }
+
+  useEffect(() => {
+    getAllGames();
+  }, []);
+
   return (
     <div>
       <h1>Hello World!</h1>
--- src/api/GameService.jsx
+++ src/api/GameService.jsx
@@ -5,3 +5,23 @@ const api_url = "http://localhost:8080/games";
 export async function saveGame(game) {
     return await axios.post(api_url, game)
 }
+
+export async function getGames(page = 0, size = 10) {
+    return await axios.get(`${api_url}?page=${page}=${size}`)
+}
+
+export async function getGame(id) {
+    return await axios.post(`${api_url}/${id}`)
+}
+
+export async function updateGame(game) {
+    return await axios.post(api_url, game)
+}
+
+export async function updatePhoto(formData) {
+    return await axios.post(`${api_url}/photo`, formData)
+}
+
+export async function deleteGame(id) {
+    return await axios.post(`${api_url}/${id}`)
+}
